f441f9f07fee1c7352e08d55d002545829aa5668
Luiz18g <94083947+Luiz18g@users.noreply.github.com>
2022-04-14 22:20:48 -0300

    atualizaçao

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lanchonete</title>
+</head>
+<body>
+    <h1>sistema de gestão de restaurante</h1>
+    <a href="#" class="botao1">cadastro de produtos</a>
+    <a href="#" class="botao2">cadastro de clientes</a>
+    <title>SGLanchonete</title>
+    <link rel="stylesheet" href="estilos.css">
+</body>
+</html>
